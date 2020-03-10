55d1e57590784f2c294cb123513304013ae8313e
Aida Abdikulova <abdikulova@gmail.com>
2020-03-10 06:18:41 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -15,6 +15,7 @@ class PetsController < ApplicationController
     if params["owners"]["name"] != nil
       @pet.owner = Owner.create(name: params["owners"]["name"])
     end
+    binding.pry
     redirect to "pets/#{@pet.id}"
   end
 
