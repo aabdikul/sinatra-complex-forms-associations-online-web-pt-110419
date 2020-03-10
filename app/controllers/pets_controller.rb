4ce3a211e6965d44f1f905824be9b127dbbafd3a
Aida Abdikulova <abdikulova@gmail.com>
2020-03-10 06:46:56 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -32,6 +32,7 @@ class PetsController < ApplicationController
 
   patch '/pets/:id' do
     @pet = Pet.find(params[:pet])
+    binding.pry
     @pet.update(params["pet"])
 
     redirect to "pets/#{@pet.id}"
