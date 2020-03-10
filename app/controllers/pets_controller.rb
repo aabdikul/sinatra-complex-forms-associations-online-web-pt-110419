ac613c7347332584c3bb2b29a2a874184bd33daf
Aida Abdikulova <abdikulova@gmail.com>
2020-03-10 06:03:20 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -17,7 +17,7 @@ class PetsController < ApplicationController
 
   get '/pets/:id' do
     @pet = Pet.find(params[:id])
-    if !params["owner"]["name"].empty?
+    if !params["owners"]["name"].empty?
       @pet.owner << Owner.create(name: params["owner"]["name"])
     end
     erb :'/pets/show'
